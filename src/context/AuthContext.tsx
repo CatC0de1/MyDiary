d6933d179ea20157b13938465f9625f91a86edfa
Iyan Zuli Armanda <catcode0101@gmail.com>
2025-07-10 16:00:25 +0700

    feat: add session, auto logout inactivity

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,20 +1,44 @@
-import { createContext, useContext, useState, ReactNode, useEffect } from "react";
+import { createContext, useContext, useState, useEffect, useRef, ReactNode   } from "react";
 
 type AuthContextType = {
   isLoggedIn: boolean;
   login: () => void;
-  // logout: () => void;
+  logout: () => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  // const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null); // if use NodeJS environment
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null); // if use browser environment (universal)
 
   useEffect(() => {
     const stored = localStorage.getItem('loggedIn');
     setIsLoggedIn(stored === 'true');
   }, []);
+
+  useEffect(() => {
+    if (!isLoggedIn) return;
+
+    const resetTimer = () => {
+      if (timeoutRef.current) clearTimeout(timeoutRef.current);
+
+      timeoutRef.current = setTimeout(() => { 
+        logout()
+      }, 2 * 60 * 1000); // 10 minutes
+    };
+
+    const activityEvents = ['click', 'keydown', 'mousemove', 'touchstart', "scroll"];
+    activityEvents.forEach(event => window.addEventListener(event, resetTimer));
+
+    resetTimer();
+
+    return () => {
+      if (timeoutRef.current) clearTimeout(timeoutRef.current);
+      activityEvents.forEach(event => window.removeEventListener(event, resetTimer));
+    };
+  }, [isLoggedIn]);
 
   const login = () => {
     localStorage.setItem('loggedIn', 'true');
@@ -24,10 +48,11 @@ export function AuthProvider({ children }: { children: ReactNode }) {
   const logout = () => {
     localStorage.removeItem('loggedIn');
     setIsLoggedIn(false);
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
   }
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, login }}>
+    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
